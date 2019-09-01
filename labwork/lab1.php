434c1e3c0ffdbc17d4fef981f1085fd854fa45f2
EC2 Default User <ec2-user@ip-172-31-80-27.ec2.internal>
2019-09-01 19:44:58 +0000

    homework lab1.php

--- labwork/lab1.php
+++ labwork/lab1.php
@@ -2,17 +2,49 @@
 <html>
     <head>
         <title>
-            My First PHP Page
+            Labwork 1: Hello, World!
         </title>
+        
+        <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great&display=swap"> 
+        <link href="https://fonts.googleapis.com/css?family=Old+Standard+TT&display=swap">
+      
+      <style>
+        
+        h1 {
+          font-size: 50px;
+          text-align: center;
+          color: #bb5372;
+          font-family: 'Fredericka the Great', cursive;
+        }
+        body {
+          font-family: font-family: 'Old Standard TT', serif;
+          font-size: 25px;
+          background: #f3f4ed;
+          width: 50%;
+          margin: 10px 0px 10px 380px;
+          color: #244b74;
+        }
+      </style>
     </head>
     <body>
-        <h1>Diana Hinchcliffe</h1>
-        <p></p>
+        <h1>I'm So Excited About PHP!!!</h1>
+        <p>Hi! My name is Diana. I'm really excited about taking this class but, 
+        also a little nervous. I really want to do well. I've already met some of my classmates when 
+        we were in class on Wednesday. I had to make sure I got some phone numbers. Mainly this summer 
+        my favorite thing to do was sit by the pool.</p>
+        
+        <p>This is the link to my <a href="\\https://github.com/rockondiana0416/cosw30\\">Github</a> account.
+        I'm sure we will be doing some great stuff so let's fill it up!!!</p>
         <?php
-            echo'This is my PHP beautiful paragraph'
+            /* lab1.php
+            Diana Hinchcliffe
+            September 1, 2019 */
+
+            echo '<p>Some of my favorite websites are <a href=\\"https://www.google.com/\\">Google</a>, <a href ="\\https://www.nordstromrack.com/\\">Nordstrom Rack</a>, and <a href="\\https://www.facebook.com/\\">Facebook</a>.</p>
+
+            <p>This assignment hasn't been to bad. I decided to start this in <a href=\\"https://codepen.io/rockondiana/pen/yLBoWYK?editors=1100\\">CodePen</a> 
+            and, then copy it into my AWS Classroom. I wanted to see how the page would render. I like to see how my coding looks like while doing it. It seems 
+            like I have to put my code through Github before I can see it. And why is my ?> showing up on the screen below? Also, when I put a semicolon after my paragragh closing tag that shows up too. </p>; 
         ?>
     </body>
 </html>
-<?php
-    echo'This is a string';
-?>
