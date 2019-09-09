b8a8e47d9c7eaede480be0a40f6f376bcd949b63
EC2 Default User <ec2-user@ip-172-31-80-27.ec2.internal>
2019-09-09 21:25:52 +0000

    trying to do this again

---
+++ homework/homework22/process_contact2.php
@@ -0,0 +1,29 @@
+<?
+    $type = $_POST['type'];
+    
+    if($type == 'register') {
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name']; 
+        $email = $_POST['email'];
+        $dropdown = $_POST['dropdown'];
+        $age = $_POST['age'];
+        $interests = $_POST['interests'];
+        $comments = $_POST['comments'];
+        
+        if($email == $correct_email) {
+        echo "<h1>Thank you! I'm gonna make you a lot of money<h1>";
+        echo "<p>First name: $first_name</p>";
+        echo "<p>Last name: $last_name</p>";
+        echo "<p>email: $email</p>";
+        echo "<p>Gender: $dropdown</p>";
+        echo "<p>Age: $age</p>";
+        echo "<p>Interests $interests</p>";
+        echo "<p>Your comments are very important to me! $comments</p>";
+        }
+        
+        else($email != $correct_email) {
+        echo "<p>Your information may be wrong. Please try again</p>";
+        echo '<p><a href="process_contact.php">Go back</a></p>';
+        }
+        }
+?> 
