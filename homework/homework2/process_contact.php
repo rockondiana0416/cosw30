62a1ca58b158098be1182da8560d2347dffca55f
EC2 Default User <ec2-user@ip-172-31-80-27.ec2.internal>
2019-09-11 03:17:44 +0000

    changes to php

--- homework/homework2/process_contact.php
+++ homework/homework2/process_contact.php
@@ -15,7 +15,7 @@
         
         $correct_email = 'test@test.com';
         
-        if($first_name == "first_name" && ($last_name == "last_name") && ($email == "email") && ($age == "age")) {
+        if (!empty($first_name) && !empty($last_name) && !empty($email) && !empty($age)) {
         echo "<h1>Thank you! I'm gonna make you a lot of money<h1>";
         echo "<p>First name: $first_name</p>";
         echo "<p>Last name: $last_name</p>";
@@ -25,9 +25,10 @@
         echo "<p>Interests $interests</p>";
         echo "<p>Your comments are very important to me! $comments</p>";
         
-        if (!empty($first_name) && !empty($last_name) && !empty($email) && !empty($age)) 
+        if  (empty($first_name) && empty($last_name) && empty($email) && empty($age))
         echo "<p>Your information may be wrong. Please try again</p>";
         echo '<p><a href="process_contact.php">Go back</a></p>';
+        }
         
-        }}
+    }
 ?> 
