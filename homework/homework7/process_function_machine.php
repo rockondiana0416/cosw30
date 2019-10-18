159ef2e857843cdd9028b1823e9619f9b266a072
Diana <rockondiana0416@yahoo.com>
2019-10-18 16:50:27 +0000

    Made changes to the isset to hopefunlly not get error.

--- homework/homework7/process_function_machine.php
+++ homework/homework7/process_function_machine.php
@@ -3,9 +3,19 @@
  {
  return ($a + $b);
  }
+
+    if (isset($_POST['num1'])) {
     $num1 = $_POST['num1'];
+}
+
+    if (isset($_POST['num2'])) {
     $num2 = $_POST['num2'];
-    $operator = $POST['operator'];
+}
+
+    if (isset($_POST['operator'])) {
+    $operator = $_POST['operator'];
+}
+
 ?>
 <!doctype HTML>
 <html lang="en">
@@ -19,8 +29,8 @@
             <label id="num1">Please enter a number:</label>
             <input type="text" name="num1" value="0" placeholder="Number 1">
             <br>
-            
-                    <label class="operator"></label>
+
+
                       <select id="operator" name="operator" value="operator">
                         <option label="">Please Select   </option>
                         <option label="">add (+)          </option>
@@ -29,45 +39,46 @@
                         <option label="">divide (/)    </option>
                       </select>
             <br>
-            
+
             <label id="num2">Please enter a number:</label>
             <input type="text" name="num2" value="0" placeholder="Number 2">
             <br>
-            <input type="submit" value="Calculate"></input>
+            <input type="submit" value="Calculate">
         </form>
-        
-        
+
+
 <?php
 
+
 if (!empty($num1) && !empty($num2) && !empty($operator)) {
-     
+
     switch($operator) {
         case 'add':
             echo $num1 . " + " . $num2 . " = ";
-            echo calculate($num1+$num2); 
+            echo calculate($num1+$num2);
             break;
-            
+
         case 'subtract':
             echo $num1 . " - " . $num2 . " = ";
-            echo calculate($num1-$num2); 
+            echo calculate($num1-$num2);
             break;
-            
-            
+
+
         case 'multiply':
             echo $num1 . " * " . $num2 . " = ";
-            echo calculate($num1*$num2); 
+            echo calculate($num1*$num2);
             break;
-            
-            
+
+
         case 'divide':
             echo $num1 . " / " . $num2 . " = ";
-            echo calculate($num1/$num2); 
+            echo calculate($num1/$num2);
             break;
-            
+
     }
     }
-    
-    else { 
+
+    else {
         echo "Please enter a number.";
     }
 
