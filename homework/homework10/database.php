f63fdf7d8bbcb8a836ea9bc83bae9a5c0a608182
Diana <rockondiana0416@yahoo.com>
2019-11-03 19:53:54 +0000

    creation of crud and database.php

---
+++ homework/homework10/database.php
@@ -0,0 +1,9 @@
+<?php
+// Create environment variables
+$hostname   = getenv('DB_HOSTNAME');
+$username   = getenv('DB_USERNAME_CLASS');
+$password   = getenv('DB_PW_CLASS');
+$dbname     = getenv('DB_NAME_UI');
+// Establish the connection to the database
+$connection = mysqli_connect($hostname, $username, $password, $dbname);
+?>
