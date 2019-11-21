8f64357626dfbce213fdc4300755615a4958dfce
Diana <rockondiana0416@yahoo.com>
2019-11-21 02:11:51 +0000

    homework11

---
+++ homework/homework11/includes/database.php
@@ -0,0 +1,9 @@
+<?php
+// Create environment variables
+$hostname   = getenv('DB_HOSTNAME');
+$username   = getenv('');
+$password   = getenv('');
+$dbname     = getenv('');
+// Establish the connection to the database
+$connection = mysqli_connect($hostname, $username, $password, $dbname);
+?>
