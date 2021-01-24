3bc47cac3d9cd4ae0459bc7e67508e4f489e7b70
Diana <rockondiana0416@yahoo.com>
2021-01-24 23:33:25 +0000

    Added foreach

--- labwork/lab4/generator.php
+++ labwork/lab4/generator.php
@@ -1,20 +1,19 @@
 <?php
     $numParagraphs = $_POST['paragraphs'];
     $text = $_POST['text'];
-    
     $textExplode = explode(' ', $text); //converts string to array
-    
-    
     shuffle($textExplode);
-    
     
     //Use the implode function, to turn it back into a string
     $textImplode = implode(' ', $textExplode);
 
-    
+    //Foreach
+    foreach($textExplode as $key => $value) {
+        echo "<p>$key Value: $value</p>";
+    }
+
     for($i = 0; $i < $numParagraphs; $i++) {
         // Output a paragraph
         echo "<p>$textImplode</p>";
     }
-    
 ?>
